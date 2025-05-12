e09c8dc7155a430c41ace9473266d2cc4fa3934e
pigletdashboard <pigletdashboard@outlook.com>
2025-05-13 06:19:06 +0700

    commit

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,9 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import dotenv from "dotenv"; // Added dotenv import
+
+dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 app.use(express.json());
@@ -80,7 +83,7 @@ app.use((req, res, next) => {
   server.listen({
     port,
     host: "0.0.0.0",
-    reusePort: true,
+    // Removed reusePort: true
   }, () => {
     log(`Discord Gambling Bot server running on port ${port}`);
   });
